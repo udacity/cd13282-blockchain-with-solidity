5961a4129bda76e51e5f93bcebb441ab74ef7973
Mohammad Salim <jd2474ev@gmail.com>
2024-03-08 01:33:53 -0500

    added demo code

--- lesson-1-writing-your-first-solidity-smart-contract/demos/basic-solidity-syntax/contracts/MyFirstContract.sol
+++ lesson-1-writing-your-first-solidity-smart-contract/demos/basic-solidity-syntax/contracts/MyFirstContract.sol
@@ -1,23 +1,7 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
-
-// Importing OpenZeppelin's SafeMath library for demonstration
-// import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+pragma solidity ^0.8.19;
 
 contract MyFirstContract {
-    // Using SafeMath for uint256 for demonstration
-    // using SafeMath for uint256;
-
     uint public myFavoriteNumber = 5;
-
-    // A simple function to change the value of myNumber
-    function setMyFavoriteNumber(uint _newFavoriteNumber) public {
-        myFavoriteNumber = _newFavoriteNumber;
-    }
-
-    // A function to retrieve the value of myNumber
-    function getMyFavoriteNumber() public view returns (uint) {
-        return myFavoriteNumber;
-    }
 }

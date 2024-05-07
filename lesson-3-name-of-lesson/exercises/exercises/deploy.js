9ed3b597d859e8fa36695de576c841bb5492ee30
meowyx <qafka256@gmail.com>
2024-05-07 20:36:12 +0600

    added two script

---
+++ lesson-3-name-of-lesson/exercises/exercises/deploy.js
@@ -0,0 +1,26 @@
+const { ethers } = require("hardhat");
+
+async function main() {
+  console.log("Starting deployment...");
+
+  // Get the contract factory for the DecentralizedMarketplace contract
+  const DecentralizedMarketplace = await ethers.getContractFactory(
+    "DecentralizedMarketplace"
+  );
+
+  // Deploy the contract
+  const contract = await DecentralizedMarketplace.deploy();
+
+  // Wait for it to be deployed
+  await contract.deployed();
+
+  // The contract is now deployed, and you can log its address
+  console.log(`DecentralizedMarketplace deployed successfully`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error("An error occurred during deployment:", error);
+    process.exit(1);
+  });
